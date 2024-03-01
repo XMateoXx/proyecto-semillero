6e0c89b446c569106bc828eba4bec5b5d52cf89e
Daniel Niebles <dnieblesp@gmail.com>
2024-02-29 19:19:05 -0500

    se coloco el campo confirmar contraseña y su validador

--- src/app/component/popup/popup.component.ts
+++ src/app/component/popup/popup.component.ts
@@ -4,12 +4,17 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Customer } from 'src/app/Model/Customer';
 import { MasterService } from 'src/app/service/master.service';
+import { PerfilService } from 'src/app/service/perfil.service';
+import { Perfil } from 'src/app/Model/Perfil';
 import { ToastService } from 'src/app/service/toast.service';
+import { CustomValidators } from 'src/app/custom.validators';
 
 @Component({
   selector: 'app-popup',
@@ -19,6 +24,7 @@ import { ToastService } from 'src/app/service/toast.service';
 export class PopupComponent implements OnInit {
   submit = false;
   hide = true;
+  lista_perfiles!: Perfil[];
   inputdata: any;
   idusuario: any = null;
   datausuario!: Customer;
@@ -29,30 +35,38 @@ export class PopupComponent implements OnInit {
     private ref: MatDialogRef<PopupComponent>,
     private formBuilder: FormBuilder,
     private service: MasterService,
-    private _servicioToast: ToastService
-  ) {}
+    private _servicioToast: ToastService,
+    private _servicioPerfil: PerfilService
+  ) {
+    this.cargar_perfiles();
+  }
   ngOnInit(): void {
     this.inputdata = this.data;
     if (this.inputdata.code > 0) {
       this.setpopupdata(this.inputdata.code);
     }
   }
-
+  cargar_perfiles() {
+    this._servicioPerfil.obtenerPerfiles().subscribe((res) => {
+      this.lista_perfiles = res;
+    });
+  }
   setpopupdata(code: any) {
     this.service.GetCustomerbycode(code).subscribe((item) => {
       this.idusuario = code;
-      console.log(code)
+      console.log(code);
       this.editdata = item;
       this.myform.setValue({
         usuario: this.editdata.usuario,
         contrasena: this.editdata.contrasena,
+        confirm_contrasena: '',
         nombres: this.editdata.nombres,
         apellido1: this.editdata.apellido1,
         apellido2: this.editdata.apellido2,
         tipodocumento: this.editdata.tipodocumento,
         identificacion: this.editdata.identificacion,
         telefono: this.editdata.telefono,
-        idperfil: this.editdata.idperfil
+        idperfil: this.editdata.idperfil,
       });
     });
   }
@@ -61,24 +75,42 @@ export class PopupComponent implements OnInit {
     this.ref.close('Closed using function');
   }
 
-  myform = new FormGroup({
-    usuario: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
-    contrasena: new FormControl('', [Validators.required,             Validators.minLength(6),
-      Validators.maxLength(40)]),
-    nombres: new FormControl('', [Validators.required]),
-    apellido1: new FormControl('', [Validators.required]),
-    apellido2: new FormControl('', [Validators.required]),
-    tipodocumento: new FormControl('', [Validators.required]),
-    identificacion: new FormControl('', [Validators.required]),
-    telefono: new FormControl('', [Validators.required]),
-    idperfil: new FormControl('', [Validators.required])
+  checkPasswords: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get("contrasena");
+    const password_repeat = control.get("confirm_contrasena");
+    
+    if (password && password_repeat && password.value !== password_repeat.value) {
+      
+      password_repeat.setErrors({ passwordCoincide: true });
+      return { passwordCoincide: true };
+    } else {
+      return null;
+    }
+  };
 
-  });
+  myform = new FormGroup(
+    {
+      usuario: new FormControl('', [Validators.required, Validators.email]),
+      contrasena: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(40),
+      ]),
+      confirm_contrasena: new FormControl('', [Validators.required]),
+      nombres: new FormControl('', [Validators.required]),
+      apellido1: new FormControl('', [Validators.required]),
+      apellido2: new FormControl('', [Validators.required]),
+      tipodocumento: new FormControl('', [Validators.required]),
+      identificacion: new FormControl('', [Validators.required]),
+      telefono: new FormControl('', [Validators.required]),
+      idperfil: new FormControl('', [Validators.required])
+    },
+    {validators: this.checkPasswords});
+ 
+
+
+ 
   
-
   /*   para acceder a los controles del formulario (form.controls) de la plantilla. 
   Por ejemplo, podemos conseguir username campo en la plantilla 
   usando f.username en lugar de form.controls.username. */
@@ -87,56 +119,64 @@ export class PopupComponent implements OnInit {
   }
   Saveuser() {
     this.submit = true;
-    if (this.editdata != null){
-      this.datausuario = {
-        id: this.idusuario,
-        apellido1: this.myform.value.apellido1!,
-        apellido2: this.myform.value.apellido2!,
-        contrasena: this.myform.value.contrasena!,
-        idperfil: Number(this.myform.value.idperfil!),
-        nombres: this.myform.value.nombres!,
-        identificacion: this.myform.value.identificacion!,
-        telefono: this.myform.value.telefono!,
-        usuario: this.myform.value.usuario!,
-        tipodocumento: this.myform.value.tipodocumento!
+    if (this.myform.valid){
+      if (this.editdata != null) {
+        this.datausuario = {
+          id: this.idusuario,
+          apellido1: this.myform.value.apellido1!,
+          apellido2: this.myform.value.apellido2!,
+          contrasena: this.myform.value.contrasena!,
+          idperfil: Number(this.myform.value.idperfil!),
+          nombres: this.myform.value.nombres!,
+          identificacion: this.myform.value.identificacion!,
+          telefono: this.myform.value.telefono!,
+          usuario: this.myform.value.usuario!,
+          tipodocumento: this.myform.value.tipodocumento!,
+        };
+  
+        this.service.actualizarUsuario(this.datausuario).subscribe({
+          next: (response) => {
+            //TODO implementar Toast
+            this._servicioToast.mostrarExito(
+              'Actualizado correctamente.',
+              'Aprobado',
+              1000
+            );
+            this.resertForm();
+          },
+          error: (response) => {
+            //TODO implementar Toast
+            console.log('Error al actualizar.');
+            this._servicioToast.mostrarError(
+              'Error al actualizar.',
+              'Error',
+              1000
+            );
+          },
+        });
+      } else {
+        this.service.Savecustomer(this.myform.value).subscribe({
+          next: (response) => {
+            //TODO implementar Toast
+            this._servicioToast.mostrarExito(
+              'Registrado correctamente.',
+              'Aprobado',
+              1000
+            );
+            this.resertForm();
+          },
+          error: (response) => {
+            //TODO implementar Toast
+            console.log('Error al registrar.');
+            this._servicioToast.mostrarError(
+              'Error al registrar.',
+              'Error',
+              1000
+            );
+          },
+        });
       }
-      
-      
-      this.service.actualizarUsuario(this.datausuario).subscribe({
-        next: (response) => {
-          //TODO implementar Toast
-          this._servicioToast.mostrarExito(
-            'Actualizado correctamente.',
-            'Aprobado',
-            1000
-          );
-          this.resertForm();
-         
-        },
-        error: (response) => {
-          //TODO implementar Toast
-          console.log('Error al actualizar.');
-          this._servicioToast.mostrarError('Error al actualizar.', 'Error', 1000);
-        },
-      });
-    }else {
 
-      this.service.Savecustomer(this.myform.value).subscribe({
-        next: (response) => {
-          //TODO implementar Toast
-          this._servicioToast.mostrarExito(
-            'Registrado correctamente.',
-            'Aprobado',
-            1000
-          );
-          this.resertForm();
-        },
-        error: (response) => {
-          //TODO implementar Toast
-          console.log('Error al registrar.');
-          this._servicioToast.mostrarError('Error al registrar.', 'Error', 1000);
-        },
-      });
     }
   }
   resertForm() {
@@ -145,16 +185,16 @@ export class PopupComponent implements OnInit {
     this.editdata = null;
     this.idusuario = null;
     this.datausuario = {
-      id: "0",
-      apellido1: "",
-      apellido2: "",
-      contrasena: "",
+      id: '0',
+      apellido1: '',
+      apellido2: '',
+      contrasena: '',
       idperfil: 0,
-      nombres: "",
-      identificacion: "",
-      telefono: "",
-      usuario:"" ,
-      tipodocumento: ""
+      nombres: '',
+      identificacion: '',
+      telefono: '',
+      usuario: '',
+      tipodocumento: '',
     };
   }
 }
--- src/app/service/perfil.service.ts
+++ src/app/service/perfil.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginService } from './auth/login.service';
+import { Perfil } from '../Model/Perfil';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,9 @@ export class PerfilService {
   private _baseURL = "http://localhost:8000";
   constructor(private http: HttpClient, private _loginService: LoginService) { }
 
-
-
-
-  obtenerPerfiles<perfil>(){
-
+  obtenerPerfiles():Observable<Perfil[]>{
+    let headers = this.header_format();
+    return this.http.get<Perfil[]>(`${this._baseURL}/obtener_perfils`, {headers});
   }
 
   header_format(){
