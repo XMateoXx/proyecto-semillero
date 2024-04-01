418192cad7751ef425fb8080e36ab1f6d38e80cd
Daniel Niebles <dnieblesp@gmail.com>
2024-03-31 19:14:18 -0500

    agregando crud al servicio de facultad

--- src/app/service/facultad.service.ts
+++ src/app/service/facultad.service.ts
@@ -5,21 +5,73 @@ import { Facultad } from '../Model/Facultad';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FacultadService {
-  private _baseURL = "http://localhost:8000";
-  constructor(private http: HttpClient, private _loginService: LoginService) { }
+  private _baseURL = 'http://localhost:8000';
+  constructor(private http: HttpClient, private _loginService: LoginService) {}
 
-
-
-
-  getFacultad(): Observable<Facultad[]>{
+  getFacultad(): Observable<Facultad[]> {
     let headers = this.header_format();
-    return this.http.get<Facultad[]>(`${this._baseURL}/obtener_facultades/`, {headers});
+    return this.http.get<Facultad[]>(`${this._baseURL}/obtener_facultades/`, {
+      headers,
+    });
   }
 
-  header_format(){
-    return new HttpHeaders().set('Authorization', 'Bearer ' + this._loginService.getJWT()['access_token']);
+  getFacultadByCode(code: any) {
+    let headers = this.header_format();
+    return this.http.get(`${this._baseURL}/obtener_facultad/` + code, {
+      headers,
+    });
+  }
+
+  GetFacultadActivo(): Observable<Facultad[]> {
+    let headers = this.header_format();
+    return this.http.get<Facultad[]>(
+      `${this._baseURL}/obtener_facultades_activos`,
+      { headers }
+    );
+  }
+
+  guardarFacultad(data: any) {
+    let headers = this.header_format();
+    return this.http.post(`${this._baseURL}/crear_facultad`, data, {
+      headers,
+    });
+  }
+
+
+  actualizarFacultad(facultad: Facultad) {
+    let headers = this.header_format();
+    return this.http.put(
+      `${this._baseURL}/actualizar_facultad`,
+      facultad,
+      { headers }
+    );
+  }
+
+  deshabilitarFacultad(id: any) {
+    let headers = this.header_format();
+    return this.http.put(
+      `${this._baseURL}/deshabilitar_facultad/` + id,
+      { facultad_id: id },
+      { headers }
+    );
+  }
+
+  activarFacultad(id: any) {
+    let headers = this.header_format();
+    return this.http.put(
+      `${this._baseURL}/activar_facultad/` + id,
+      { facultad_id: id },
+      { headers }
+    );
+  }
+
+  header_format() {
+    return new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + this._loginService.getJWT()['access_token']
+    );
   }
 }
